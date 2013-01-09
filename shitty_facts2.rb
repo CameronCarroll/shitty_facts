df04e792efed4931a2bd505facf4e5b5d0a58ea9
Cameron Carroll <ckcarroll4@gmail.com>
2013-01-09 03:39:48 -0800

    Added some pregfacts to yamlfile, made some logmessages more clear, added SMS content and msgs remaining logs

--- shitty_facts2.rb
+++ shitty_facts2.rb
@@ -57,21 +57,24 @@ class SMSClient
 
   def check_and_send
     
-    $log.debug "SMS counter: " + @sms_counter.to_s
+    $log.debug "# of SMS Sent: " + (@sms_counter-1).to_s  #sms_counter is incremented AFTER sending the next message
     $log.debug "Last SMS time: " + @last_sms_time.to_s if @last_sms_time
 
     # if @last_sms_time exists, then it's NOT the first message.
     # We have to check whether it's been wait_time since the last message.
     if @last_sms_time
-      wait_time_in_seconds = @wait_time * 60 * 60
+      wait_time_in_seconds = @wait_time * 60 * 60 # wait_time is given in hours
       time_difference = Time.now - @last_sms_time
-      $log.debug "Wait time (seconds): " + wait_time_in_seconds.to_s
-      $log.debug "Time difference: " + time_difference.to_s
+      $log.debug "Halt time: " + wait_time_in_seconds.to_s + " seconds"
+      $log.debug "Time since last message: " + time_difference.to_s + " seconds"
 
       if time_difference.to_i > wait_time_in_seconds
-        $log.debug "Time difference exceeded wait time. Sending next message to: " + @target_number.to_s
+        $log.debug "Time waited > " + wait_time_in_seconds + " seconds; Sending next message to: " + @target_number.to_s
         $log.info "Message sent to: " + @target_number.to_s
         send_message
+      else
+        time_remaining = wait_time_in_seconds - time_difference
+        $log.debug "Time check failed; " + time_remaining.to_s + " seconds remaining."
       end
 
     else # it is the first message.
@@ -79,6 +82,8 @@ class SMSClient
       $log.info "Starting new project for: " + @target_number.to_s
       send_message
     end
+
+    $log.debug "Unsent messages remaining: " + (@messages.length - @sms_counter-1).to_s
   end
 
   def send_message
@@ -88,6 +93,7 @@ class SMSClient
     if success_result == 'true'
       @last_sms_time = Time.now
       $log.debug "Successfully sent SmS at: " + @last_sms_time.to_s
+      $log.debug "SMS Content: `" + message + "'"
       @sms_counter = @sms_counter + 1
     else
       error = result.split(':')[2][1..-3]
